e7362617abdf6327c5057636440ef4b02530275c
priyan <45042030+priyan1995@users.noreply.github.com>
2019-07-24 23:41:47 +0530

    Jquery text animation
    Jquery text animation on scroll

---
+++ 12_jQ_animation/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+$(window).scroll(function(){
+		if($(this).scrollTop()>5){
+
+			$("#p").css({"left":"20%","top":"50%","font-size":"80px"});
+			$("#r").css({"left":"30%","top":"50%","font-size":"80px"});
+			$("#i").css({"left":"40%","top":"50%","font-size":"80px"});
+			$("#y").css({"left":"50%","top":"50%","font-size":"80px"});
+			$("#a").css({"left":"60%","top":"50%","font-size":"80px"});
+			$("#n").css({"left":"70%","top":"50%","font-size":"80px"});
+		}
+
+
+});
+
+
+
+})
+
